81bf3609423180b62f3c0655788376277e475806
lagrawal1 <loku.cs.agrawal@gmail.com>
2025-03-23 06:29:07 +0000

    Link and image extraction added.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,77 @@
 from textnode import TextNode
 from textnode import TextType
 from htmlnode import HTMLNode, LeafNode
+import re
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.extend(node)
+            continue
+
+        if node.text.count(delimiter)%2 != 0:
+            raise Exception("All markdown delimiters must be closed.")
+        
+        text_list = node.text.split(delimiter)
+        node_list = []
+        for i in range(len(text_list)):
+            if i%2 == 0:
+                node_list.append(TextNode(text_list[i], TextType.TEXT))
+            elif i%2 == 1:
+                node_list.append(TextNode(text_list[i], text_type))
+        
+        new_nodes.extend(node_list)
+    return new_nodes           
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        images = extract_markdown_images(node.text)
+
+        if len(images) == 0:
+            new_nodes.extend(node)
+            continue
+
+        text_list, node_list = [], []
+        for image in images:
+            text_list = re.split(r"!\[.*?\]\(.*?\)", node.text)
+        
+        for i in range(len(text_list) + len(images) - 1):
+            if i%2 == 0:
+                node_list.append(TextNode(text_list[int(i/2)], TextType.TEXT))
+            elif i%2 == 1:
+                node_list.append(TextNode(images[int((i-1)/2)][0], TextType.IMAGE, images[int((i-1)/2)][1]))
+                
+        new_nodes.extend(node_list)
+    return new_nodes
+
+            
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        links = extract_markdown_links(node.text)
+
+        if len(links) == 0:
+            new_nodes.extend([node])
+            continue
+
+        text_list, node_list = [], []
+        for image in links:
+            text_list = re.split(r"\[.*?\]\(.*?\)", node.text)
+        
+        for i in range(len(text_list) + len(links) - 1):
+            if i%2 == 0:
+                node_list.append(TextNode(text_list[int(i/2)], TextType.TEXT))
+            elif i%2 == 1:
+                node_list.append(TextNode(links[int((i-1)/2)][0], TextType.LINK, links[int((i-1)/2)][1]))
+                
+        new_nodes.extend(node_list)
+    return new_nodes
+
+
+
 
 def text_node_to_html_node(text_node):
     match text_node.text_type:
@@ -16,7 +87,15 @@ def text_node_to_html_node(text_node):
             return LeafNode("a" , text_node.text, props = {"href" : text_node.url})
         case TextType.IMAGE:
             return LeafNode("a" , value = "", props = {"src" : text_node.url , "alt" : text_node.text})
-            
+
+def extract_markdown_images(text):
+    image_tuples = re.findall(r"!\[(.*?)\]\((.*?)\)" , text)
+    return image_tuples
+
+def extract_markdown_links(text):
+    link_tuples = re.findall(r"\[(.*?)\]\((.*?)\)" , text)
+    return link_tuples
+              
 def main():
     test = TextNode("testing za text", TextType.BOLD, "idk.com")
     print(test)
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,7 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
-from main import text_node_to_html_node
+from main import *
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -35,6 +35,66 @@ class TestTextNodetoHTML(unittest.TestCase):
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, "This is a text node")
+    
+    
+
+class TestMDtoTextNode(unittest.TestCase):
+    def test_text_code(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE) 
+        self.assertEqual(new_nodes, [
+    TextNode("This is text with a ", TextType.TEXT),
+    TextNode("code block", TextType.CODE),
+    TextNode(" word", TextType.TEXT)
+    ])  
+
+class TestImageExtractMD(unittest.TestCase):
+    def test_img_extract(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        self.assertEqual([("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")], extract_markdown_images(text))
+    
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+        
+class TestSplitNodes(unittest.TestCase):
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+    def test_split_links(self):
+        node = TextNode(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual([
+TextNode("This is text with a link ", TextType.TEXT),
+TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+TextNode(" and ", TextType.TEXT),
+TextNode(
+         "to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"
+     ),
+ ] ,new_nodes )
+
+
+
 
 
 if __name__ == "__main__":
